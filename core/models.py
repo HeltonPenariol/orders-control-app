725c4495b92ccaa4dcb3f4f71fdec45ede93044c
Mauricio <mauriciofagner2010@hotmail.com>
2020-02-23 20:44:09 -0300

    Model the Item object

--- core/models.py
+++ core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    class Meta:
+        verbose_name = 'Iten'
+
+    PRODUTOS = (
+        ('Pizza Grande', 'Pizza Grande'),
+        ('Pizza Grande (2 Sabores)', 'Pizza Grande (2 Sabores)'),
+        ('Pizza Pequena', 'Pizza Pequena'),
+        ('Pizza Pequena (2 Sabores)', 'Pizza Pequena (2 Sabores)'),
+        ('Esfiha Aberta', 'Esfiha Aberta'),
+        ('Esfiha Fechada', 'Esfiha Fechada'),
+        ('Cheeseburguer', 'Cheeseburguer'),
+        ('Porção', 'Porção'),
+        ('Pastel', 'Pastel'),
+        ('Beirute', 'Beirute'),
+        ('Sobremesa', 'Sobremesa'),
+        ('Bebida', 'Bebida'),
+    )
+
+    tipo_produto = models.CharField(verbose_name='Tipo do Produto', max_length=255, choices=PRODUTOS)
+    nome = models.CharField(verbose_name='Produto', max_length=255)
+    preco = models.DecimalField(verbose_name='Valor do Produto', max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.tipo_produto} - {self.nome} - Valor: {self.preco}'
+    
