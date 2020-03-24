9ca7fa9b4c07622eaa84b5074ff929b84c331c73
Mauricio <mauriciofagner2010@hotmail.com>
2020-03-24 18:12:16 -0300

    Create modeling of orders placed at the counter

--- core/models.py
+++ core/models.py
@@ -114,8 +114,8 @@ class Bebida(models.Model):
 
 class Pedido(models.Model):
     class Meta:
-        verbose_name = 'Pedido'
-        verbose_name_plural = 'Pedidos'
+        verbose_name = 'Pedido - Delivery'
+        verbose_name_plural = 'Pedidos - Delivery'
 
     STATUS = (
         ('Em preparo', 'Em preparo'),
@@ -156,6 +156,95 @@ class Pedido(models.Model):
 
     def __str__(self):
         return f'PEDIDO: #{self.user} - {self.rua} - {self.horario_recebimento}'
+
+    def get_valor_pedido(self):
+        total = 0
+
+        for pizza_grande in self.pizzas_grande.all():
+            total += pizza_grande.get_valor_item()
+        
+        for pizza_broto in self.pizzas_broto.all():
+            total += pizza_broto.get_valor_item()
+
+        for esfiha in self.esfihas.all():
+            total += esfiha.get_valor_item()
+        
+        for lanche in self.lanches.all():
+            total += lanche.get_valor_item()
+        
+        for sobremesa in self.sobremesas.all():
+            total += sobremesa.get_valor_item()
+    
+        for bebida in self.bebidas.all():
+            total += bebida.get_valor_item()
+
+        return total
+
+    def get_valor_total(self):
+        total = 0
+        total += self.taxa_entrega
+        total += self.taxa_adicional
+        total -= self.desconto
+
+        for pizza_grande in self.pizzas_grande.all():
+            total += pizza_grande.get_valor_item()
+        
+        for pizza_broto in self.pizzas_broto.all():
+            total += pizza_broto.get_valor_item()
+
+        for esfiha in self.esfihas.all():
+            total += esfiha.get_valor_item()
+        
+        for lanche in self.lanches.all():
+            total += lanche.get_valor_item()
+        
+        for sobremesa in self.sobremesas.all():
+            total += sobremesa.get_valor_item()
+    
+        for bebida in self.bebidas.all():
+            total += bebida.get_valor_item()
+
+        return total
+
+class PedidoBalcao(models.Model):
+    class Meta:
+        verbose_name = 'Pedido - Balcão'
+        verbose_name_plural = 'Pedidos - Balcão'
+
+    STATUS = (
+        ('Em preparo', 'Em preparo'),
+        ('Em entrega', 'Em entrega'),
+        ('Concluído', 'Concluído'),
+    )
+
+    PAGAMENTOS = (
+        ('Cartão', 'Cartão'),
+        ('Dinheiro', 'Dinheiro'),
+    )
+
+    CONCLUSOES_PAGAMENTO = (
+        ('Dinheiro', 'Dinheiro'),
+        ('Débito', 'Débito'),
+        ('Crédito', 'Crédito'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    nome = models.CharField(verbose_name="Identificação", max_length=255, blank=True)
+    pizzas_grande = models.ManyToManyField(PizzaGrande, blank=True)
+    pizzas_broto = models.ManyToManyField(PizzaBroto, blank=True)
+    esfihas = models.ManyToManyField(Esfiha, blank=True)
+    lanches = models.ManyToManyField(Lanche, blank=True)
+    sobremesas = models.ManyToManyField(Sobremesa, blank=True)
+    bebidas = models.ManyToManyField(Bebida, blank=True)
+    desconto = models.DecimalField(verbose_name='Descontos', max_digits=3, decimal_places=2, default=0)
+    pagamento_conclusao = models.CharField(verbose_name='Conclusão do Pagamento', max_length=255, choices=CONCLUSOES_PAGAMENTO, blank=True)
+    horario_recebimento = models.DateTimeField(verbose_name='Horário de Recebimento', auto_now_add=True)
+    horario_atualizacao = models.DateTimeField(verbose_name='Horário de Atualização', auto_now=True)
+    status = models.CharField(verbose_name='Status do Pedido', max_length=255, choices=STATUS, default='Em preparo')
+    observacao = models.TextField(verbose_name='Observações do Pedido', blank=True)
+
+    def __str__(self):
+        return f'PEDIDO: #{self.user} {self.nome} - {self.horario_recebimento}'
 
     def get_valor_pedido(self):
         total = 0
